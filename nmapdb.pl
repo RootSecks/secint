76b7a7dfabe2accf75f3f925519e7a5365f93272
RootSecks <RootSecks@RootSecks.com>
2013-09-11 21:26:26 +1200

    Updated nmapdb.pl to respect the conf file. Removed some printed noise

--- nmapdb.pl
+++ nmapdb.pl
@@ -8,31 +8,56 @@ use DBI();
 
 
 my $np = new Nmap::Parser;
-my $nmap_exe = '/usr/bin/nmap';
-#my $scan_arg_default = '--dns-servers 192.168.17.221 -O -sV -p 21,22,23,69,25,53,80,161,443,445,1433,3306,3389,5900';
-#my $scan_arg_default = '--dns-servers 192.168.17.221 -O -sV';
-#my $scan_arg_default = '';
-#my $scanrange = "192.168.17.200-254";
 
 
+my $nmap_exe = "";
+my $dbhost = " ";
+my $dbname = " ";
+my $dbuser = " ";
+my $dbpass = " ";
+
+open SECINTCONF, "secint.conf" or die $!;
+
+my @confline;
+
+while (<SECINTCONF>) {
+
+	chomp;
+	
+	if ($_ ne "") {
+		
+		my @confline = split('=', $_);
+		
+		if ($confline[0] eq "nmapbin") {
+			$nmap_exe = $confline[1];		
+		} elsif ($confline[0] eq "host") {
+			$dbhost = $confline[1];
+		} elsif ($confline[0] eq "database") {
+			$dbname = $confline[1];
+		} elsif ($confline[0] eq "user") {
+			$dbuser = $confline[1];
+		} elsif ($confline[0] eq "password") {
+			$dbpass = $confline[1];
+		}
+
+	}
+
+}
+
+close SECINTCONF or die $!;;
 
 
+if (!defined $dbpass) {
+	$dbpass = '';
+}
 
-my $dbh = DBI->connect("DBI:mysql:database=sect;host=localhost",
-                         "root", "",
+	
+
+my $dbh = DBI->connect("DBI:mysql:database=$dbname;host=$dbhost",
+                         "$dbuser", "$dbpass",
                          {'RaiseError' => 1});
 
-#my $scanarg = $scan_arg_default;
-
-
-#my $allargs = $scanarg . " " . $scanrange;
-
 my $allargs = shift;
-
-
-print "Scanning with the following: \r\n";
-print "nmap " . $allargs . "\r\n";	
-
 
 $np->parsescan($nmap_exe, $allargs);
 
@@ -40,32 +65,36 @@ my $npsession = $np->get_session();
 
 my $scantime = $npsession->finish_time() - $npsession->start_time();
 
-print $scantime . "\r\n";
-
-$dbh->do("INSERT INTO SectintScans (ScanTime, ScanDuration, ScanOptions, ScanType) VALUES (NOW(), \"" . $scantime . "\", \"" . $allargs . "\", \"nmap\")");
+$dbh->do("INSERT INTO SecintScans (ScanTime, ScanDuration, ScanOptions, ScanType) VALUES (NOW(), \"" . $scantime . "\", \"" . $allargs . "\", \"nmap\")");
 
 my $lastscanid = $dbh->selectrow_array('SELECT LAST_INSERT_ID()');
 
 for my $host ($np->all_hosts()){
-
-	print "Host: " . $host->addr() . " \r\n ";
 
 	my $hoststatus = $host->status();
 	my $hostaddr = $host->addr();
 	my $hostname = $host->hostname();
 	my $hostos = $host->os_sig();
 	my $hostosname = $hostos->name();	
+	
+	if (!defined $hostos) {
+		$hostos = '';
+	}
+
+	if (!defined $hostosname) {
+		$hostosname = '';	
+	}
+
+	if (!defined $hostname) {
+		$hostname = '';
+	}
 
 	$dbh->do("INSERT INTO NmapHosts (HostName, HostIP, ParentScan, HostStatus, HostOS) VALUES (\"" . $hostname . "\", \"" . $hostaddr . "\", " . $lastscanid . ", \"" . $hoststatus . "\", \"" . $hostosname . "\")");
 
 
 	my $parenthostid = $dbh->selectrow_array('SELECT LAST_INSERT_ID()');
 
-	print "Ports: \r\n";
-
 	for my $port ($host->tcp_ports('open')) {
-
-		print "\r\nPort: $port \r\n";
 
 		my $serviceproto = $host->tcp_service($port)->proto();
 		my $serviceport = $port;
@@ -101,13 +130,6 @@ for my $host ($np->all_hosts()){
 
 		}
 
-
-		print "Service Proto: " . $serviceproto;
-		print "\r\nService FP: " . $servicefingerprint;
-		print "\r\nService Product: " . $serviceproduct;
-		print "\r\nService Version: " . $serviceversion;
-
-		print "\r\n inserting service on port: " . $port . " into database under parent host: " . $parenthostid . "\r\n";		
 
 		$dbh->do("INSERT INTO NmapServices (ServiceProto, ServicePort, ServiceName, ParentHost, ServiceFingerPrint, ServiceProduct, ServiceVersion) VALUES (\"" . $serviceproto . "\", \"" . $serviceport . "\", \"" . $servicename . "\", $parenthostid , \"" . $servicefingerprint . "\", \"" . $serviceproduct . "\", \"" . $serviceversion . "\")");
 	
