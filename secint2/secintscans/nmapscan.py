869f38a94a43a4eb94042a30f176b2c60e70057c
RootSecks <RootSecks@RootSecks.com>
2013-10-04 22:26:54 +1300

    removing some old files

--- secint2/secintscans/nmapscan.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import nmap
-
-class NmapScan():
-    def __init__(self, options):
-        pipe = subprocess.Popen(["./nmapdb.pl", options], stdout=subprocess.PIPE)
-        result = pipe.stdout.read()
-        return result
